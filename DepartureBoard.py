3d6ec3b3f028daab17e763145106e6417d493071
Conrad Ronk <conradronk@gmail.com>
2019-08-11 17:59:20 -0700

    Trying the google gtfs_realtime_pb2 library

--- DepartureBoard.py
+++ DepartureBoard.py
@@ -1,3 +1,12 @@
-from gtfspy import TransitData
+from google.transit import gtfs_realtime_pb2
+import urllib.request
 
-td = TransitData("return.xml")
+API_key = ""
+stop_ID = "6849"
+
+feed = gtfs_realtime_pb2.FeedMessage()
+response = urllib.request.urlopen("https://developer.trimet.org/ws/V1/arrivals?locIDs=" + stop_ID + "&appID=" + API_key)
+feed.ParseFromString(response.read())
+for entity in feed.entity:
+  if entity.HasField('trip_update'):
+    print(entity.trip_update)
